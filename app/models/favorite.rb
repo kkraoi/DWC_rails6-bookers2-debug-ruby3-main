86f4591f6d5e8760ddc73e4e08e4728b25c2818d
aoi_takenaka <aaoooii219@gmail.com>
2025-05-05 03:03:43 +0000

    [Update]新規モデルにバリデーションを設定

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,7 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :book
+
+  # {scope: :post_image_id} <= post_image_idごとに
+  validates user_id, uniqueness: {scope: :post_image_id}
 end

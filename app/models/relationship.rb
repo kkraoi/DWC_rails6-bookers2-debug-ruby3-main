6825cadafa0c5e6ec8cd0e7b0fe477f665f80b07
aoi_takenaka <aaoooii219@gmail.com>
2025-05-06 08:15:20 +0000

    [Update]アソシエーション設定

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,7 @@
 class Relationship < ApplicationRecord
+  # フォローされているユーザー：followerという関連名としてUserモデルと関連付ける。実態としてfollower_idを外部キーとして、Userモデルと参照を行う。
+  belongs_to :follower, class_name: "User"
+
+  # フォローしているユーザー
+  belongs_to :followed, class_name: "User"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,18 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
+  # フォローしている関連づけ：active_relationshipsという関連名で、follower_idを外部キーとしてRelationshipモデルと関連づけ。
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+
+  # フォローされている関連付け
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  # フォローしているユーザーを参照: followingsという関連名でactive_relationshipsを経由してfollowedと関連づける
+  has_many :followings, through: :active_relationships, source: :followed
+
+  # フォロワーを参照
+  has_many :followers, through: :passive_relationships, source: :follower
+
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
   

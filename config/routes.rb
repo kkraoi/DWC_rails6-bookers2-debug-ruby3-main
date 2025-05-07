5e919a767c012c0db0f4a509e38dab3541c2a905
aoi_takenaka <aaoooii219@gmail.com>
2025-05-07 01:39:55 +0000

    [Update]ルーティング調整

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,7 @@ Rails.application.routes.draw do
     get "followings" => "relationships#followings"
   	get "followers" => "relationships#followers"
   end
+
+  get "/search", to: "searches#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

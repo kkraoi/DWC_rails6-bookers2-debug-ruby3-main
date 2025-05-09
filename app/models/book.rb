17579c2299f8b131da54363da0e7b690e7994c30
aoi_takenaka <aaoooii219@gmail.com>
2025-05-09 07:40:35 +0000

    [Update]いいねが多い順に並び替える

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,13 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+
+  # Favoriteクラス(モデル)からwhereメソッドで1週間分のレコードを取ってきて、week_favoritesという関連名で登録。
+  # (Time.current.at_end_of_day - 6.day).at_beginning_of_day: 6日前の日付の開始時刻（00:00:00）を取得する
+  # Time.current.at_end_of_day: 現時刻の終わりの時刻（23:59:59）を取得する
+  # 「7日間」ではなく「6日間+今日」 の範囲になる
+  has_many :week_favorites, -> { where(created_at: ((Time.current.at_end_of_day - 6.day).at_beginning_of_day)..(Time.current.at_end_of_day)) }, class_name: 'Favorite'
+  
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
 

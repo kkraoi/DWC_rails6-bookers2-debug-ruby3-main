b501e31631dd666e678f8bab2f7313559293ef47
aoi_takenaka <aaoooii219@gmail.com>
2025-05-09 07:42:16 +0000

    [Fix]不要記述削除

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,11 +12,12 @@ class BooksController < ApplicationController
   end
 
   def index
-    to = Time.current.at_end_of_day
-    from = (to - 6.day).at_beginning_of_day
-    @books = Book.includes(:favorites).sort_by { |book|
-      -book.favorites.where(created_at: from..to).count 
-    }
+    # コントローラで1週間絞り込みをする場合
+    # to = Time.current.at_end_of_day
+    # from = (to - 6.day).at_beginning_of_day
+    # @books = Book.includes(:favorites).sort_by { |book|
+    #   -book.favorites.where(created_at: from..to).count 
+    # }
 
     @books =  Book.includes(:favorites).sort_by { |book|
       -book.week_favorites.count
